55a57b72ec4b627eb6bd077b63d40324fa1f220d
ThuongDoo <domanhthuong20122002@gmail.com>
2023-08-28 16:12:16 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    title: 'Portfolio',
+    imgUrl: "https://github.com/ThuongDoo/images/blob/main/footerbg.jpg?raw=true",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non, ullam illo? Consectetur omnis fugiat magni minima quaerat enim facilis iure repellendus ipsam ea tenetur nobis, eum natus, possimus, unde excepturi.",
+    technologies: ["React Navigation", "TailwindCSS", "Expo", "Redux", "Sanity"],
+  },
+  {
+    title: 'Game Store',
+    imgUrl: "https://play-lh.googleusercontent.com/AQtSF5Sl18yp3mQ2tcbOrBLekb7cyP3kyg5BB1uUuc55zfcnbkCDLHFTBwZfYiu1aDI",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non, ullam illo? Consectetur omnis fugiat magni minima quaerat enim facilis iure repellendus ipsam ea tenetur nobis, eum natus, possimus, unde excepturi.",
+    technologies: ["React Navigation", "TailwindCSS", "Expo", "Redux", "Sanity"],
+  },
+  {
+    title: 'Delivery App',
+    imgUrl: "https://play-lh.googleusercontent.com/AQtSF5Sl18yp3mQ2tcbOrBLekb7cyP3kyg5BB1uUuc55zfcnbkCDLHFTBwZfYiu1aDI",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non, ullam illo? Consectetur omnis fugiat magni minima quaerat enim facilis iure repellendus ipsam ea tenetur nobis, eum natus, possimus, unde excepturi.",
+    technologies: ["React Navigation", "TailwindCSS", "Expo", "Redux", "Sanity"],
+  },
+]
